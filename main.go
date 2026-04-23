305b27e6a0fa994e10ae6ea5ff5f69151f38f115
Marcus Rothsluk <mrothsluk@users.noreply.github.com>
2026-04-23 22:52:36 +0800

    docs: add personal fork context to package comment

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 // Package main is the entry point for the powertracker application.
 // powertracker monitors and tracks power consumption data from smart meters.
+//
+// This is a personal fork of github.com/poolski/powertracker.
+// Primary use case: monitoring home solar + grid usage on a Raspberry Pi.
 package main
 
 import "github.com/poolski/powertracker/cmd"
